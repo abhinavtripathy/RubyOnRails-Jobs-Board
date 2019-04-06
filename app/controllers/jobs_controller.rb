ef1caddbd36b245bde395eea857deb027cf5a480
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-04-06 02:34:57 -0400

    Jobs Controller Update, Updating SCSS Files with Booststrap

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,39 @@
+class JobsController < ApplicationController
+    def index
+    end
+
+    def show 
+    end 
+
+    def new 
+        @job = Job.new
+    end
+
+    def create
+        @job = Job.new(jobs_params)
+        if @job.save
+            redirect_to @job
+        else 
+            render "New"
+        end
+    end 
+
+    def edit
+    end 
+
+    def update
+    end
+
+    def destroy 
+    end
+
+    private 
+
+    def jobs_params
+        params.require(:job).premit(:title, :description, :company, :url)
+    end
+
+    def find_jobs
+        @job = Job.findparams([:id])
+    end
+end
