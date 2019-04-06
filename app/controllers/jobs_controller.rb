648d6834f08efd9140dffaba921e3f4454341c5b
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-04-06 02:53:56 -0400

    Jobs Controller Update & Job Views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,8 @@
 class JobsController < ApplicationController
+    before_action :find_job, only:[:show, :edit, :update, :destroy]
+
     def index
+        @jobs = Job.all.order("created at desk")
     end
 
     def show 
